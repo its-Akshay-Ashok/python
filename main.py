0b88edb82085df12e69ebc8427897bf6e3dd9d4c
dhanishton <83818934+dhanishton@users.noreply.github.com>
2021-05-07 17:08:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+number = 911
+msg = "call this number for help{}"
+print(msg.format(number))
+
+name = "albert"
+age = 34
+percentage = 96.5
+message = "my name is{} i am {} year old my percentage is {}%"
+print(message.format(name,age,percentage))
